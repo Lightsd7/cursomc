a58d354e0168411227caa15b417bd940930c6843
Raphael Molina <raphael.bedran@konduto.com>
2020-05-12 19:20:42 -0300

    CRUD Categoria
    Criação do CategoriaDTO.

--- src/main/java/com/molina/cursomc/services/CategoriaService.java
+++ src/main/java/com/molina/cursomc/services/CategoriaService.java
@@ -1,30 +1,49 @@
 package com.molina.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.molina.cursomc.domain.Categoria;
 import com.molina.cursomc.repositories.CategoriaRepository;
+import com.molina.cursomc.services.exceptions.DataIntegrityException;
 import com.molina.cursomc.services.exceptions.ObjectNotFoundException;
-
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
-	private CategoriaRepository repo; 
-	
-//	public Categoria buscar(Integer id) {
-//		Optional<Categoria> obj = repo.findById(id);
-//		return obj.orElse(null);
-//	}
-	
-	public Categoria buscar(Integer id) {
+	private CategoriaRepository repo;
+
+	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
-						"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+	}
+
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
+
+	public Categoria update(Categoria obj) {
+		find(obj.getId());
+		return repo.save(obj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		}catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
+		}
 	}
 	
+	public List<Categoria> findAll() {
+		return repo.findAll();
+	}
 }
